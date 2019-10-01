ed9019379942311176bb45abb45eb9301e1e5362
Samuel Williams <williamssamuel1999@gmail.com>
2019-10-01 13:23:33 +0100

    Added emitter.unregisterEvent, program_manager.EmitterProxy

--- client/js/base/program_manager.js
+++ client/js/base/program_manager.js
@@ -1,12 +1,24 @@
 define(["console", "emitter"], function(console, emitter) {
 
+	class EmitterProxy {
+		constructor(name) {
+			this._name = name;
+			this._eventNames = [];
+		}
+		registerEvent(event) {
+			emitter.registerEvent(event);
+
+		}
+	}
+
 	class Program {
-		constructor(name, aliases, desc, usage) {
+		constructor(name, aliases = [], desc = "Unknown", usage = "Unknown") {
 			this._name = name;
 			this._aliases = aliases;
 			this._desc = desc;
 			this._usage = usage;
-		},
+			this.emitter = new EmitterProxy(name);
+		}
 		onStart(f) {
 			this._start = f;
 		}
@@ -23,9 +35,8 @@ define(["console", "emitter"], function(console, emitter) {
 
 		stop(errCode) {
 			this._stop(errCode);
-			
+			this.emitter.removeAll()
 		}
-
 	}
 
 
--- client/js/libs/emitter.js
+++ client/js/libs/emitter.js
@@ -5,36 +5,45 @@ function eventExists(event) {
 	return eventNames.indexOf(event) != -1;
 }
 
-define({
-	registerEvent: function(event) {
-		if(eventExists(event)) {
-			throw "Event " + event + " already exists";
-		}
-		eventNames.push(event);
-		events[event] = {};
-	},
-	emit: function(event, ...args) {
-		if(eventExists(event)) {
-			for(let key in events[event]) {
-				events[event][key](...args);
+define(["helper"], function(helper) {
+	return {
+		registerEvent: function(event) {
+			if(eventExists(event)) {
+				throw "Event " + event + " already exists";
 			}
-		} else {
-			throw "Event " + event + " does not exist";
-		}
-	},
-	on: function(event, id, cb) {
-		if(eventExists(event)) {
-			events[event][id] = cb;
-		} else {
-			throw "Event " + event + " does not exist";
-		}	
-	},
-	remove: function(event, id) {
-		if(eventExists(event)) {
-			delete events[event][id]
-		} else {
-			throw "Event " + event + " does not exist";
-		}
-	},
-	getTable: function() { return events; }
+			eventNames.push(event);
+			events[event] = {};
+		},
+		unregisterEvent: function(event) {
+			if(!eventExists(event)) {
+				throw "Event " + event + " does not exist";
+			}
+			eventNames.removeByValue(event);
+			delete events[event];
+		},
+		emit: function(event, ...args) {
+			if(eventExists(event)) {
+				for(let key in events[event]) {
+					events[event][key](...args);
+				}
+			} else {
+				throw "Event " + event + " does not exist";
+			}
+		},
+		on: function(event, id, cb) {
+			if(eventExists(event)) {
+				events[event][id] = cb;
+			} else {
+				throw "Event " + event + " does not exist";
+			}	
+		},
+		remove: function(event, id) {
+			if(eventExists(event)) {
+				delete events[event][id]
+			} else {
+				throw "Event " + event + " does not exist";
+			}
+		},
+		getTable: function() { return events; }
+	}
 })
